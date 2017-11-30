261ef0dee4db5e398b5660ae32889ba3c9393e10
tomii <tomii@m.u-tokyo.ac.jp>
2017-11-30 02:17:33 +0000

    correct size

--- rfdata.py
+++ rfdata.py
@@ -9,17 +9,18 @@ class RFdata(object):
         self.dt = dt          # sampling interval
         self.c = c            # sound velocity
         
-        assert self.data.shape[0] == len(src)
-        assert self.data.shape[1] == len(rcv)
+        self.n_src, self.n_rcv, self.n_time = list(self.data.shape)
+        self.n_pos, self.n_dim = list(self.pos.shape)
         
-        self.mesh_src, self.mesh_rcv = np.meshgrid(src, rcv)
+        assert self.n_src == len(src)
+        assert self.n_rcv == len(rcv)
+        assert self.n_src <= self.n_pos
+        assert self.n_rcv <= self.n_pos
         
-        self.pos_src = pos[self.mesh_src.flatten()].reshape(
-            np.array([self.mesh_src.shape[0], self.mesh_src.shape[1], pos.shape[1]])
-        )
-        self.pos_rcv = pos[self.mesh_rcv.flatten()].reshape(
-            np.array([self.mesh_rcv.shape[0], self.mesh_rcv.shape[1],  pos.shape[1]])
-        )
+        self.mesh_rcv, self.mesh_src = np.meshgrid(rcv, src)
+        
+        self.pos_src = pos[self.mesh_src.flatten()].reshape(self.n_src, self.n_rcv, self.n_dim)
+        self.pos_rcv = pos[self.mesh_rcv.flatten()].reshape(self.n_src, self.n_rcv, self.n_dim)
         
     def relevant_submat(self, target):
         dist_src = np.linalg.norm( self.pos_src - target, axis= 2)
