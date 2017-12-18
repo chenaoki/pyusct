46f725f6fada4543f7ca1c0d535fa333dc2441e1
tomii <tomii@m.u-tokyo.ac.jp>
2017-12-18 14:06:21 +0000

    getPointSubset

--- rfdata.py
+++ rfdata.py
@@ -22,8 +22,28 @@ class RFdata(object):
         self.pos_src = pos[self.mesh_src.flatten()].reshape(self.n_src, self.n_rcv, self.n_dim)
         self.pos_rcv = pos[self.mesh_rcv.flatten()].reshape(self.n_src, self.n_rcv, self.n_dim)
         
-    def relevant_submat(self, target):
+        self.mat_time = np.zeros_like(self.data, dtype=np.int16)
+        for i in range(self.mat_time.shape[2]) : self.mat_time[:,:,i] = i
+        
+    def getArrivalTimeArray(self, target):
+        
         dist_src = np.linalg.norm( self.pos_src - target, axis= 2)
         dist_rcv = np.linalg.norm( self.pos_rcv - target, axis = 2)
         dist = dist_src + dist_rcv
         return (dist/(self.c*self.dt)).astype(np.uint16)
+    
+    def getPointSubset(self, target, offset_array):
+        
+        ret = np.zeros((self.data.shape[0], self.data.shape[1],len(offset_array)))
+        
+        time_arrive = self.getArrivalTimeArray(target)
+        mat_time_diff = self.mat_time - time_arrive[:,:,np.newaxis]
+        
+        for i, offset in enumerate(offset_array):
+            mat_filter = np.ones_like(self.mat_time)
+            mat_filter *= (mat_time_diff == offset)*1
+            rf_filtered = self.data * mat_filter
+            map_filtered =np.sum(rf_filtered, axis=2)
+            ret[:,:,i] = map_filtered
+        
+        return ret
